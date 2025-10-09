3210877114126ff06d051ca28f551c37b144a7be
Ted <ted@boy.local>
2025-10-08 23:45:04 -0700

    got rid of annoying error

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TungTungTungSahur.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/TungTungTungSahur.java
@@ -2,7 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 public class TungTungTungSahur {
     public TungTungTungSahur () {
-        PuyallupLEDController ledController = new PuyallupLEDController(Tung Tung Tung);
-        ledController.setByte((byte) (67));
+        //PuyallupLEDController ledController = new PuyallupLEDController("Tung", "Tung", "Tung" );
+        //ledController.setByte((byte) (67));
     }
 }
